aac43e6f8db7587e10121f2d2cf97c2b903bdc2c
shubham2408-alt <shubham@sensorflow.org>
2025-06-02 22:13:41 +0530

    Update s3.tf

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -2,25 +2,6 @@ resource "aws_s3_bucket" "this" {
   bucket        = local.s3_bucket_name
   force_destroy = var.force_destroy_s3_bucket
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.s3.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
   tags = merge(
     var.standard_tags,
     {
@@ -38,4 +19,14 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_acls       = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
