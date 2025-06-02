8a140c339696443c4f8606f68314fb94d1cff329
shubham2408-alt <shubham@sensorflow.org>
2025-06-02 21:59:11 +0530

    Update s3.tf

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -10,6 +10,16 @@ resource "aws_s3_bucket" "this" {
       }
     }
   }
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
 
   tags = merge(
     var.standard_tags,
@@ -19,11 +29,7 @@ resource "aws_s3_bucket" "this" {
   )
 }
 
-# ✅ New resource for ACL (instead of using deprecated inline attribute)
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
+
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
